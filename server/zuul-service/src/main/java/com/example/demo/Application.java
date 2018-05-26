e6316397e1775db7172fe0f3a4045cb49cc54609
ksb <rnjstjdqhd3939@gmail.com>
2018-05-26 16:39:37 +0900

    test

--- server/zuul-service/src/main/java/com/example/demo/Application.java
+++ server/zuul-service/src/main/java/com/example/demo/Application.java
@@ -2,9 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableEurekaServer
+@EnableZuulProxy
+@EnableEurekaClient
 @SpringBootApplication
 public class Application {
 
