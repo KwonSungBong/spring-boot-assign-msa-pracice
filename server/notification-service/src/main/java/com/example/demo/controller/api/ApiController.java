a65733404f85c4047a4d82123cf0979f1388476a
ksb <rnjstjdqhd3939@gmail.com>
2018-05-26 18:01:22 +0900

    test

---
+++ server/notification-service/src/main/java/com/example/demo/controller/api/ApiController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/notification")
+public class ApiController {
+
+    @GetMapping
+    public String test() {
+        return "notification";
+    }
+
+}
---
+++ server/user-service/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,14 @@
+package com.example.demo.entity;
+
+public class User {
+    //audience
+    String name;
+    //audience
+    String gender;
+    //audience
+    String birth;
+    //audience
+    String address;
+    //audience
+    String interest;
+}
